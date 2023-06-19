0e8d40a7fced6dfd72cb3cd0a6f69357c50bc8be
Patrick Melo <patrickabimael@yahoo.com.br>
2023-06-19 01:13:51 -0300

    first commit

---
+++ src/main/java/com/example/sarasartsecommerce/SarasartsecommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.sarasartsecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SarasartsecommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SarasartsecommerceApplication.class, args);
+	}
+
+}
